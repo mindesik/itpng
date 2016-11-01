c49dafbe118462d1e085c020de18e394cd1754d4
De Sik Min <e.min@milax.com>
2016-11-02 10:59:33 +1100

    Publish

---
+++ README.md
@@ -0,0 +1,13 @@
+## Installation
+
+Install the package
+
+```
+$ npm i -g itinypng
+```
+
+Set tiny png api key
+
+```
+$ TINYPNG_API_KEY=yourapikey > ~/.itinypng
+```
---
+++ bin/itinypng.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var tinify = require('tinify');
+var env = require('dotenv').config({path: process.env.HOME + '/.itinypng'});
+var extensions = ['png', 'jpg', 'jpeg'];
+
+if (!env.TINYPNG_API_KEY) {
+    return console.log('TINYPNG_API_KEY is not set');
+}
+
+tinify.key = env.TINYPNG_API_KEY;
+
+var args = process.argv.slice(2);
+
+var toConvert = [];
+
+fs.readdirSync(process.cwd()).forEach(function (file) {
+    var parts = file.split('.');
+    if (parts.length > 1) {
+        if (extensions.indexOf(parts[parts.length - 1]) !== -1) {
+            var path = process.cwd() + '/' + file;
+            toConvert.push(path);
+        }
+    }
+});
+
+if (toConvert.length > 0) {
+    toConvert.forEach(function (file) {
+        var source = tinify.fromFile(file);
+        var parts = file.split('.');
+        var extension = parts.pop();
+        source.toFile(parts.join('.') + '_compressed' + '.' + extension);
+    });
+    console.log('Done converting ' + toConvert.length + ' files!');
+} else {
+    console.log('No files to convert');
+}
